90ad21523571c694d523fd61d0917e3b64bd992d
Fantast1k <83510907+HSFantast1k@users.noreply.github.com>
2021-10-21 11:46:52 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-
+    $('.slider').slick({
+        prevArrow: '<button class="slick-arrow slick-left"><img src="images/arrow-left.svg" alt="left arrow"></button>',
+        nextArrow: '<button class="slick-arrow slick-right"><img src="images/arrow-right.svg" alt="right arrow"></button>',
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                }
+            }
+        ]
+    });
 });
